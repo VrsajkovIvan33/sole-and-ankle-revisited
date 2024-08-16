a48ab187103ecbd0dad2fee6fe5c88e43212ecb4
Ivan Vrsajkov <vrsajkov.ivan@gmail.com>
2024-08-16 13:58:46 +0200

    add initial version of mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -13,23 +13,44 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <DialogOverlay>
+      <DialogContent>
+        <DialogLayout>
+          <button onClick={onDismiss}>Dismiss menu</button>
+          <DialogNav>
+            <a href="/sale">Sale</a>
+            <a href="/new">New&nbsp;Releases</a>
+            <a href="/men">Men</a>
+            <a href="/women">Women</a>
+            <a href="/kids">Kids</a>
+            <a href="/collections">Collections</a>
+          </DialogNav>
+          <DialogFooter>
+            <a href="/terms">Terms and Conditions</a>
+            <a href="/privacy">Privacy Policy</a>
+            <a href="/contact">Contact Us</a>
+          </DialogFooter>
+        </DialogLayout>
+      </DialogContent>
+    </DialogOverlay >
   );
 };
+
+const DialogLayout = styled.div`
+  height: 100%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+`;
+
+const DialogNav = styled.nav`
+  display: flex;
+  flex-direction: column;
+`;
+
+const DialogFooter = styled.footer`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default MobileMenu;
