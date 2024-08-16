7dd142a9c45a7058082c2b7ce833b607c9b3b2fe
Ivan Vrsajkov <vrsajkov.ivan@gmail.com>
2024-08-16 12:21:10 +0200

    add top border to header when on mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
@@ -46,28 +46,32 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media ${QUERIES["tabletAndSmaller"]} {
+    border-top: 4px solid ${COLORS.gray[900]};
+  }
 `;
 
 const Nav = styled.nav`
-  display: flex;
-  gap: 48px;
-  margin: 0px 48px;
+display: flex;
+gap: 48px;
+margin: 0px 48px;
 `;
 
 const Side = styled.div`
-  flex: 1;
+flex: 1;
 `;
 
 const NavLink = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${COLORS.gray[900]};
-  font-weight: ${WEIGHTS.medium};
+font - size: 1.125rem;
+text - transform: uppercase;
+text - decoration: none;
+color: ${COLORS.gray[900]};
+font - weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: ${COLORS.secondary};
-  }
+  &: first - of - type {
+  color: ${COLORS.secondary};
+}
 `;
 
 export default Header;
