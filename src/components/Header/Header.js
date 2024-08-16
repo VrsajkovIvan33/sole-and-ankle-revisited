288381aeac2f1379953165b42028d15488dbf2b3
Ivan Vrsajkov <vrsajkov.ivan@gmail.com>
2024-08-16 12:51:22 +0200

    add functionality to open burger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -42,7 +42,7 @@ const Header = () => {
         <UnstyledButton>
           <Icon id="search" strokeWidth={2} />
         </UnstyledButton>
-        <UnstyledButton>
+        <UnstyledButton onClick={() => setShowMobileMenu(true)}>
           <Icon id="menu" strokeWidth={2} />
         </UnstyledButton>
       </SmallScreenMainHeader>
